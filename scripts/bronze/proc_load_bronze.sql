a087bdc04c515234f25bb3ee58b3ae1c8a84b640
tharrmeehan <58373200+tharrmeehan@users.noreply.github.com>
2025-03-22 09:41:59 +0100

    Modified Stored Procedure and Data Flow Diagram
    - Modified data flow diagram by adding silver layer
    - Stored proc silver

--- scripts/bronze/proc_load_bronze.sql
+++ scripts/bronze/proc_load_bronze.sql
@@ -3,14 +3,13 @@
 Stored Procedure: Load Bronze Layer (Source -> Bronze)
 ====================================================================================
 Script Purpose:
-This stored procedure loaqf data into the 'bronze' schema from external CSV files.
+This stored procedure loads data into the 'bronze' schema from external CSV files.
 It performs the following actions:
     - Truncates the bronze tables before loading data.
     - Uses the 'COPY' command to load data from csv Files to bronze tables.
 
 Parameters:
-    None.
-    This stored procedure does not accept any parameters or return any values.
+    None. This stored procedure does not accept any parameters or return any values.
 
 Usage Example:
     call bronze.load_bronze();
